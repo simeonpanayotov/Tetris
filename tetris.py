72572a620698b8a2b5dfe6ad5c42a889cc1c8e10
Simeon Panayotov <simeon.panayotov@gmail.com>
2012-09-06 21:00:31 +0300

    Rename the grid module to grids.

--- tetris.py
+++ tetris.py
@@ -1,17 +1,17 @@
 import pygame
-import grid
+import grids
 import panels
 
 TETRIS = "Tetris"
 BG_COLOR = (219, 203, 138)
 PANEL_WIDTH = 150
 PANEL_HEIGHT = 200
-WIDTH = grid.SCREEN_WIDTH + PANEL_WIDTH
+WIDTH = grids.SCREEN_WIDTH + PANEL_WIDTH
 
 class Tetris:
     def __init__(self):
     # Create the game window.
-        self._screen = pygame.display.set_mode((WIDTH, grid.SCREEN_HEIGHT))
+        self._screen = pygame.display.set_mode((WIDTH, grids.SCREEN_HEIGHT))
         pygame.display.set_caption(TETRIS)
 
         # Create the background surface.
@@ -19,10 +19,10 @@ class Tetris:
         self._background = self._background.convert()
         self._background.fill(BG_COLOR)
 
-        self._gameGrid = grid.Grid()
+        self._gameGrid = grids.Grid()
         self._nextShapePanel = panels.NextShapePanel(
             PANEL_WIDTH, PANEL_HEIGHT,
-            grid.SCREEN_WIDTH, 0)
+            grids.SCREEN_WIDTH, 0)
 
 
     def update(self):
